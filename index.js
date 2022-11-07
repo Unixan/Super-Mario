605130d116d363caf87e8f627c9d517ab36c04c6
Unixan <115973884+Unixan@users.noreply.github.com>
2022-11-07 14:53:42 +0100

    WIP

---
+++ index.js
@@ -0,0 +1,17 @@
+// Model
+
+
+// View
+function view(){
+    document.getElementById('app').innerHTML = /*HTML*/ `
+        <div class="selectChar">
+            <img src="IMG/mario.png" class="char"/>
+            <img src="IMG/luigi.png" class="char"/>
+            <img src="IMG/peach.png" class="char"/>
+            <img src="IMG/yoshi.png" class="char"/>
+        </div>
+    `
+}
+
+
+// Controller
